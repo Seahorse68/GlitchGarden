d373cd658e7b88fca7052516de1f0bfc05feda1f
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-09-02 23:11:01 +0100

    Glitch Garden with Menus Complete

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            //print("Level auto load disabled");
+        }
+        else
+        {
+            // Invoke waits for autoLoadNextLevelAfter seconds, then calls LoadNextLevel
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray = new AudioClip[8];
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsManager.GetMasterVolume();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic;
+        thisLevelMusic = levelMusicChangeArray[level];
+
+        if (thisLevelMusic)
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
