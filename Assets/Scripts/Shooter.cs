86afc3956a816eab1489b80cd4ebc5828279b288
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-09-21 14:17:31 +0100

    End of Video 164

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 
 public class Shooter : MonoBehaviour {
-    public GameObject projectile, projectileParent, gun;
+    public GameObject projectile, gun;
+
+    private GameObject projectileParent;
 
 	// Use this for initialization
 	void Start () {
-	
+        projectileParent = GameObject.Find("Projectiles");
+
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject("Projectiles");
+        }
 	}
 	
 	// Update is called once per frame
