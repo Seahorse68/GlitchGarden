40f5b2a22c2d10ca3bf00f6da5d440af69dd358b
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-09-22 13:56:57 +0100

    End of Video 166

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,21 +5,72 @@ public class Shooter : MonoBehaviour {
     public GameObject projectile, gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
 	// Use this for initialization
 	void Start () {
+        animator = GameObject.FindObjectOfType<Animator>();
+
+        // creates a parent if necessary
         projectileParent = GameObject.Find("Projectiles");
 
         if (!projectileParent)
         {
             projectileParent = new GameObject("Projectiles");
         }
+
+        SetMyLaneSpawner();
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if (IsAttackerAheadInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        } else {
+            animator.SetBool("isAttacking", false);
+        }
 	}
+
+    // look thru all spawners and set myLaneSpawner if found
+    void SetMyLaneSpawner()
+    {
+        Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+        foreach (Spawner spawner in spawnerArray)
+        {
+            if (spawner.transform.position.y == transform.position.y)
+            {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+
+        Debug.LogError(name + " can't find spawner in lane");
+    }
+
+    bool IsAttackerAheadInLane()
+    {
+        // exit if no attackers in lane
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        // if there are attackers, are they ahead?
+        foreach (Transform attacker in myLaneSpawner.transform)
+        {
+            if (attacker.transform.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+
+        // attacker in lane, but behind us
+        return false;
+    }
 
     private void Fire()
     {
